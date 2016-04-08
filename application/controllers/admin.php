cb7565c42b421b6cb04b7d5bf24ceaf29e767ba8
Adrian Bean <Adrian@BeanIT.local>
2016-04-08 18:21:12 +0100

    created basic admin controller and function templates to be implemented.

---
+++ application/controllers/admin.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function index() {}
+
+	public function login() {}
+
+	public function logout() {}
+
+	public function import_users() {}
+
+	public function list_users() {}
+
+	public function update_user() {}
+}
